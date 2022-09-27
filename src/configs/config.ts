55c932d4b21615121ff8b9ae0ecdfd026db81d67
AlinaSyniavska <alina22syniavska@gmail.com>
2022-09-27 09:44:23 +0300

    fix mongo url

--- src/configs/config.ts
+++ src/configs/config.ts
@@ -1,8 +1,5 @@
-const dbUrl = 'mongodb+srv://user:LcEkMOfi2FssRzRF@cluster0.rdkochd.mongodb.net/radency-note-app?retryWrites=true&w=majority';
-
 export const config = {
     PORT: process.env.PORT || 5001,
     NODE_ENV: process.env.NODE_ENV,
-    // MONGO_URL: process.env.MONGO_URL || 'mongodb://localhost:27017/test',
-    MONGO_URL: dbUrl || 'mongodb://localhost:27017/test',
+    MONGO_URL: process.env.MONGO_URL || 'mongodb://localhost:27017/test',
 };
